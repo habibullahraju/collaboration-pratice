9bded9c69bf6c6a7427d4339f62a9c64e142f9f6
riyad3399 <programminghero.riyad@gmail.com>
2023-08-09 01:48:39 +0600

    new dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite  hello and React logos to learn more
-        <form action="">
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-      </form>
-      </p>
-    </>
-  )
-}
+    <div>
+      <h3>new development</h3>
+      <h3>new development</h3>
+      <h3>new development</h3>
+    </div>
+  );
+};
 
-export default App
+export default App;
